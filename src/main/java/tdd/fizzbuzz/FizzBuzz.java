cdc25598af3cc379f037f4ce0c773e94e6545c11
Marek Kalmarczyk <Marek.Kalmarczyk@orange.com>
2020-07-15 01:47:48 +0200

    Test 3,5

--- src/main/java/tdd/fizzbuzz/FizzBuzz.java
+++ src/main/java/tdd/fizzbuzz/FizzBuzz.java
@@ -2,4 +2,7 @@ package tdd.fizzbuzz;
 
 public class FizzBuzz {
 
+    public String fizzBuzz(Integer num) {
+        return num.toString();
+    }
 }
--- src/test/java/tdd/fizzbuzz/FizzBuzzTest.java
+++ src/test/java/tdd/fizzbuzz/FizzBuzzTest.java
@@ -1,13 +1,48 @@
 package tdd.fizzbuzz;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class FizzBuzzTest {
 
+    FizzBuzz fizzBuzz;
+
+    @BeforeEach
+    void setUp() {
+        fizzBuzz = new FizzBuzz();
+    }
+
     @Test
-    public void startHere() {
-        assertThat(4).isEqualTo(5);
+    public void returnNumber1(){
+
+        String fizzBuzzRes = fizzBuzz.fizzBuzz(1);
+
+        assertThat(fizzBuzzRes).isEqualTo("1");
+    }
+
+    @Test
+    void returnNumber2() {
+
+        String fizzBuzzRes = fizzBuzz.fizzBuzz(2);
+
+        assertThat(fizzBuzzRes).isEqualTo("2");
+    }
+
+    @Test
+    void returnNumber3() {
+
+        String fizzBuzzRes = fizzBuzz.fizzBuzz(3);
+
+        assertThat(fizzBuzzRes).isEqualTo("Fizz");
+    }
+
+    @Test
+    void returnNumber5() {
+
+        String fizzBuzzRes = fizzBuzz.fizzBuzz(5);
+
+        assertThat(fizzBuzzRes).isEqualTo("Buzz");
     }
 }
